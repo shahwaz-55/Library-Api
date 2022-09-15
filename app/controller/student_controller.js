25d5c1dc424fd02d13dd127a9f9377afbca7bcf9
shahwaz alam <shahwazait7303@gmail.com>
2022-09-15 23:57:59 +0530

    commit ittu sa

---
+++ app/controller/student_controller.js
@@ -0,0 +1,29 @@
+const book = require('../model/student_model');
+const http_formatter = require('../util/http_formatter');
+
+
+const Newuser = async (request, response) => {
+    try {
+        const {pageNo, perPage} = request.query;
+        const users = await book.find({}).skip(perPage*(pageNo-1)).limit(perPage);
+        return response.status(200).json(http_formatter(users,'users got succesfully'));
+    }
+    
+    catch (error) {
+        console.log(error);
+        return response.status(400).json(http_formatter(error, "Something went wrong, please try again", false));
+      
+    }
+}
+const createbook = async (request, response) => {
+        try {
+            const users = await book.create(request.body);
+            return response.status(201).json( http_formatter(users, 'user created successfully'))} 
+        catch ( err ) {
+            return response.status(400).json(http_formatter(err, 'something went wrong, please try again', false));
+        }
+}
+module.exports = {
+    getbooks,
+    createbook,
+}
